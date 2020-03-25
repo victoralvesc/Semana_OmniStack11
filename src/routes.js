fe0394796d8b779db4a4fe01613b095ec87afec1
Victor Alves <victor.alvescosta@outlook.com>
2020-03-25 07:55:15 -0300

    configurando rotas, npx, banco e etc

---
+++ src/routes.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const routes = express.Router();
+
+routes.post('/users', (request, response) => {
+    const body = request.body;    
+    
+    console.log(body);
+    
+    return response.json ({
+        evento: 'Semana OmniStack11',
+        aluno: 'Victor Antonio Alves'
+    });
+}); /*rota raiz do node*/
+
+module.exports = routes;
