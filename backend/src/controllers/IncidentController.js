af2fb9d9a8ca120926d6ec8b9b65a22caac95caa
Victor Alves <victor.alvescosta@outlook.com>
2020-03-26 09:14:25 -0300

    Correção arquivo IncidentController

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -1,6 +1,12 @@
 const connection = require('../database/connection');
 
-module.exports ={
+module.exports = {
+    async index (request, response) {
+        const incidents = await connection('incidents').select('*');
+        
+        return response.json(incidents);      
+    },
+
     async create(request, response) {
         const { title, description, value } = request.body;
         const ond_id = request.headers.authorization;
@@ -13,5 +19,23 @@ module.exports ={
         });
 
         return response.jso({ id });
-    }  
+    },
+    
+    async delete(request, response) {
+        const {id} = request.params;
+        const ong_id = request.headers.authorization
+
+        const incident = await connection('incidents')
+        .where('id', id)
+        .select('ong_id')
+        .first();
+
+        if (incident.ong_id !== ong_id) {
+            return response.status(401).json({ erro: 'Operation not permitted.'});
+        }
+
+        await connection('incidents').where('id', id).delete();
+            return response.status(204).send();
+    }
+
 };
