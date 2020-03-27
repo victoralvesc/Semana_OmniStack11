9d11a98b27ea8094f105e7c641b1f240ec87a400
Victor Alves <victor.alvescosta@outlook.com>
2020-03-27 12:24:25 -0300

    Finalizando todo o processo de estilização do Frontend

--- frontend/src/pages/logon/index.js
+++ frontend/src/pages/logon/index.js
@@ -16,7 +16,7 @@ export default function Logon() {
                 <h1>Faça seu Logon</h1>
 
                 <input placeholder="Informe seu ID" />
-                <button type="submit">Entrar</button>
+                <button className="button" type="submit">Entrar</button>
                 <a href="/register" >
                     <FiLogIn size={16} color="#E02041" />
                     Não tenho Cadastro
