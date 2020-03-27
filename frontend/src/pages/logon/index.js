07e3c956a218599f954199a8ffe4ab2bea69c914
Victor Alves <victor.alvescosta@outlook.com>
2020-03-27 09:21:21 -0300

    estilizando o front com arquivo css, instalou react-icones

--- frontend/src/pages/logon/index.js
+++ frontend/src/pages/logon/index.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import {FiLogIn} from 'react-icons/fi';
 
 import './styles.css';
 
+import logoImg from '../../assets/logo.svg';
+import heroesImg from '../../assets/heroes.png';
+
 export default function Logon() {
     return (
-        <h1>Hello Word</h1>
+       <div className= "Logon-container">
+           <section className="form">
+
+           <img src={logoImg} alt="Be The Hero" />
+            <form>
+                <h1>Faça seu Logon</h1>
+
+                <input placeholder="Informe seu ID" />
+                <button type="submit">Entrar</button>
+                <a href="/register" >
+                    <FiLogIn size={16} color="#E02041" />
+                    Não tenho Cadastro
+                </a>
+            </form>
+           </section>
+   
+            <img src={heroesImg} alt="Heroes" />
+       </div>
     ); 
 }
